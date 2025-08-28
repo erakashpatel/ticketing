94ce49780d9f901876fbb3dd0885c6c8a620aa6c
erakashpatel <akash_mac@d2:ff:65:1c:35:59.home>
2025-08-28 14:00:22 -0400

    Intial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Hello from Vue in Laravel 🎉</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
+ 
